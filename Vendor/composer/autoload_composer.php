c100b8326a8bdab227caaba141b35fc3b53d7798
luzijia <luzijia@gmail.com>
2017-12-06 15:43:45 +0800

    composer

---
+++ Vendor/composer/autoload_composer.php
@@ -0,0 +1,11 @@
+<?php
+$vendorDir = dirname(dirname(__FILE__));
+$baseDir = dirname($vendorDir);
+
+return[
+
+    'Vendor\\Blade\\' => $vendorDir . '/blade/src/',
+
+    'Symfony\\Component\\HttpFoundation\\' => $vendorDir . 'http-foundation/',
+
+];

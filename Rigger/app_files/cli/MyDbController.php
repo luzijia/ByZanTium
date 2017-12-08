dc9e6b29520a0a8b34d3c1d9fe52f798e6510d89
luzijia <luzijia@gmail.com>
2017-12-08 15:59:59 +0800

    fix

--- Rigger/app_files/cli/MyDbController.php
+++ Rigger/app_files/cli/MyDbController.php
@@ -20,11 +20,6 @@ class MyDbController extends BaseController
         $db_config = $db_config['DEFAULT'];
         $DB = new MySqlBase($db_config['DB_HOST'], $db_config['DB_NAME'], $db_config['DB_USER'], $db_config['DB_PASSPORT']);
 
-        $DB->beginTransaction();
-        $rows = $DB->query("delete from user");
-        $DB->commit();
-
-        $rows = $DB->query("insert user set uid=?,nickname=?,addtime=?",array(51,"gg",date("Ymd H:i:s")));
         $rows = $DB->getOne("SELECT * FROM user WHERE uid = ? ", array(51));
         var_dump($rows);
     }
